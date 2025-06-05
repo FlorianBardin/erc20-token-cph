869e81af29c39763af29d504d652afae27892c98
Florian Bardin <fbardin51@gmail.com>
2025-06-05 20:54:33 +0200

    Création du contrat Cephee et implémentation de son constructeur

--- contracts/Cephee.sol
+++ contracts/Cephee.sol
@@ -2,3 +2,9 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Cephee is ERC20 {
+    constructor(uint256 initialSupply) ERC20('Cephee', 'CPH'){
+        _mint(msg.sender, initialSupply * (10**decimals()));
+    }
+}
